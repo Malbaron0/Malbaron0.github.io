d78d8d76a69ca9ffce93fc1efac3429b00cbdee4
Malyar Baron <malbaron0@gmail.com>
2017-02-10 18:54:36 -0500

    Personal Website

---
+++ personal-website.js
@@ -0,0 +1,20 @@
+
+
+/* Stop the video from playing modal is closed. */
+$("#myModal").on("hidden.bs.modal", function() {
+    myvid = document.getElementById('myVideo');
+    myvid.pause();
+});
+
+/*Set android mp4 as the sourse of the video when Launch demo within Android Application is clicked */
+$("#android-app-ref").click(function(){
+  document.getElementById('myVideo').setAttribute("src", "https://player.vimeo.com/video/203526410");
+});
+
+$("#bookscanner-app-ref").click(function(){
+  document.getElementById('myVideo').setAttribute("src", "https://player.vimeo.com/video/203526011");
+});
+
+$("#punchclock-app-ref").click(function(){
+  document.getElementById('myVideo').setAttribute("src", "https://player.vimeo.com/video/203523141");
+});
